8ee0db15df459ae46ec5da7d740065298cd7624e
Vincent Sakougui <vincent.sakouvogui@epitech.eu>
2023-02-22 10:21:11 +0100

    Security Change

--- security.php
+++ security.php
@@ -2,10 +2,11 @@
 /**
  * 
  */
+
 class Security 
 {
 	
-	function __construct(argument)
+	function __construct()
 	{
 		// code...
 	}
@@ -14,21 +15,65 @@ class Security
 		//detect malicious log 
 		$texte=filter_var($parametre, FILTER_SANITIZE_STRING);
 		$patterns = array();
-		$patterns[0] = '/%2B/';
-		$patterns[1] = '/x3e/';
-		$patterns[2] = '/x3c/';
-		$patterns[3] = '/HTTPS/';
-		$patterns[4] = '/:///';
-		$patterns[5] = '/https/';
-		$patterns[6] = '/www./';
+		$patterns[0] = "/%2B/";
+		$patterns[1] = "/x3e/";
+		$patterns[2] = "/x3c/";
+		$patterns[3] = "/HTTPS/";
+		$patterns[4] = "/:///";
+		$patterns[5] = "/https/";
+		$patterns[6] = "/www./";
 		// $patterns[2] = '/renard/';
-		$texte=preg_replace($patterns, '', $texte,-1)
+		$texte=preg_replace($patterns, '', $texte,-1);
 		return $texte;
+	}
+
+function str_replace_assoc(array $replace, $subject) {
+
+   return str_replace(array_keys($replace), array_values($replace), $subject);    
+
+}
+	
+		
+	function againstXSS($parametre){
+		$texte=filter_var($parametre, FILTER_SANITIZE_STRING);
+		
+		$replace = array(
+
+
+			'script' => '',
+			'&gt' => '',
+			'&lt' => '',
+			'&quot' => '',
+			'&#039' => '',
+			'script' => '',
+			'http' => '',
+			'www' => '',
+			'script' => '',
+			'document.location' => '',
+			'document.cookie' => '',
+			'onmouse' => '',
+			'3e' => '',
+			'<' => '',
+			'>' => '',
+			'3c' => '',
+			'%73%63%72%69%70%74' => '',
+			'3c%73%63%72%69%70%74%3' => '',
+			'69%70%74%3' => '',
+			'%2B' => '',
+			'x3e' => '',
+			'x3c' => '',
+			'63%72' => '',
+			"/%3c%2f%73%63%72%69%70%74%3e/" => ''
+
+			);
+	
+		return str_replace(array_keys($replace), array_values($replace), $texte);
+		
 	}
 
 	function CheckNumberInt($parametre){
 		if(isset($parametre) && is_int($parametre)){
-			$result= filter_var($parametre, FILTER_SANITIZE_NUMBER_INT)
+			$result= filter_var($parametre, FILTER_SANITIZE_NUMBER_INT);
 			return $result;
 		}
 
@@ -36,7 +81,7 @@ class Security
 	}
 	function CheckNumber($parametre){
 		if(isset($parametre) && is_int($parametre)){
-			$result= filter_var($parametre, FILTER_SANITIZE_NUMBER_INT)
+			$result= filter_var($parametre, FILTER_SANITIZE_NUMBER_INT);
 			return $result;
 		}
 
@@ -55,4 +100,19 @@ class Security
 	}
 }
 
+$objectCreated= new Security();
+//\x3cscript\x3ealert(1)\x3c/script\x3e
+//on
+// echo$objectCreated->checkmatch("<script>alert(1);</script> script %22");
+
+// $lname=$_GET['lname'];
+$fname=$_GET['fname'];
+echo$fname;
+
+echo"Using the againstXSS function => firstname is: <br>";
+echo$objectCreated->againstXSS($fname);
+// echo"<br> Using the againstXSS function => lastname is: <br>";
+// echo$objectCreated->againstXSS($lname);
+
 ?>
+
---
+++ securityui.php
@@ -0,0 +1,77 @@
+<?php
+
+  header("Content-Security-Policy: default-src 'self';");
+  header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+  header("X-FRAME-OPTIONS: DENY");
+  header("X-Content-Type-Options: nosniff");
+  header("X-XSS-Protection: 1; mode=block");
+  header("Referrer-Policy: no-referrer-when-downgrade");
+  header("X-Download-Options: noopen");
+  header("X-Permitted-Cross-Domain-Policies: none");
+  header("Expect-CT: max-age=0, enforce");
+
+  // Store the current time in a variable
+$current_time = time();
+
+// Check if the IP address is in the rate limit cache
+if (!isset($_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']])) {
+  // If the IP address is not in the cache, initialize the rate limit data
+  $_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']] = [
+    'requests' => 0,
+    'timestamp' => $current_time,
+  ];
+}
+
+// Increment the number of requests for this IP address
+$_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']]['requests']++;
+
+// Check if the number of requests is within the limit
+if ($_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']]['requests'] > 100) {
+  // If the number of requests is greater than the limit, check the time difference
+  $time_difference = $current_time - $_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']]['timestamp'];
+  if ($time_difference < 3600) {
+    // If the time difference is less than an hour, return an error message
+    header("HTTP/1.1 429 Too Many Requests");
+    exit("Too many requests. Please try again later.");
+  } else {
+    // If the time difference is greater than an hour, reset the rate limit data
+    $_SESSION['rate_limit_cache'][$_SERVER['REMOTE_ADDR']] = [
+      'requests' => 0,
+      'timestamp' => $current_time,
+    ];
+  }
+}
+
+  
+setcookie('cookie', rand(100, 999), 0, '/', '', false, true)
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://*; child-src 'none';" />
+  <meta charset="UTF-8">
+  <meta name="description" content="Stock Platform">
+  <!-- <meta name="keywords" content="HTML, CSS, JavaScript"> -->
+  <meta name="author" content="Vincent Sakouvogui">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head> 
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="security.php" method="GET">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"  ><br>
+  <!-- <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br> -->
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+<script >
+
+  alert(11);
+</script>
+</body>
+</html>
